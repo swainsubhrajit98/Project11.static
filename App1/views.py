a6d7d35b1ea5312168ea447e86a613abcdae9247
Swagat Swain <ssswagatss@gmail.com>
2022-10-07 19:21:31 +0530

    first commit

---
+++ App1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Insert(request):
+    return render(request,'Insert.html')
+def Second(request):
+    return render(request,'Second.html')
